78e5a5dc22253f4d04c8a1e453b36b052d2127dc
pdabacus <dayalpavan@gmail.com>
2019-10-18 20:50:27 -0400

    first run

--- 2.lexical_addition/main.cpp
+++ 2.lexical_addition/main.cpp
@@ -1,13 +1,16 @@
 #include <cstdio>
 #include <iostream>
 
-#define VERBOSE
+//#define VERBOSE
 
 int main(int argc, char* argv[]) {
     unsigned int N, a, b; // target num
     unsigned int n;
     unsigned int max_len;
     unsigned int min, max;
+    unsigned int left;
+    unsigned int num_b;
+    unsigned int k;
 
     std::cin >> N >> a >> b;
     #ifdef VERBOSE
@@ -23,6 +26,23 @@ int main(int argc, char* argv[]) {
         #endif
         if (min <= N && N <= max) {
             printf("YES\n");
+            left = N - min;
+            num_b = left/(b-a);
+            left -= (b-a)*num_b;
+            // (n - num_b - 1) a's
+            // left + a
+            // (num_b) b's
+            #ifdef VERBOSE
+                printf("num_b=%u, left=%u\n", num_b, left);
+            #endif
+            for (k=0; k<n-num_b-1; ++k) {
+                printf("%u ", a);
+            }
+            printf("%u ", a + left);
+            for (k=0; k<num_b; ++k) {
+                printf("%u ", b);
+            }
+            printf("\n");
             return 0;
         }
     }
