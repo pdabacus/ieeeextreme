dc395cf688db2b6f966d9e04f5314859f22b5678
pdabacus <dayalpavan@gmail.com>
2019-10-18 20:36:45 -0400

    unfinished lexical

--- 2.lexical_addition/main.cpp
+++ 2.lexical_addition/main.cpp
@@ -4,12 +4,28 @@
 #define VERBOSE
 
 int main(int argc, char* argv[]) {
-    unsigned int n, a, b;
-    unsigned int j;
+    unsigned int N, a, b; // target num
+    unsigned int n;
+    unsigned int max_len;
+    unsigned int min, max;
 
-    std::cin >> n >> a >> b;
+    std::cin >> N >> a >> b;
     #ifdef VERBOSE
-        printf("n=%u, a=%u, b=%u\n", n, a, b);
+        printf("N=%u, a=%u, b=%u\n", N, a, b);
     #endif
+
+    max_len = N / a;
+    for (n=1; n<=max_len; ++n) {
+        min = n * a;
+        max = n * b;
+        #ifdef VERBOSE
+            printf("trying length %u: %u <= N <= %u\n", n, min, max);
+        #endif
+        if (min <= N && N <= max) {
+            printf("YES\n");
+            return 0;
+        }
+    }
+    printf("NO\n");
     return 0;
 }
